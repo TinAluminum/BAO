25c9dc35e03ee0a5e75d44a641700f0e360923c4
TinAluminum <panask338@gmail.com>
2022-10-10 19:32:29 -0700

    Init pygame bsaic screen

---
+++ constants.py
@@ -0,0 +1,5 @@
+import pygame
+
+
+background_color = (200, 200, 200)
+screen_size = (1000, 600)
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import pygame
-print('Hello')
+import constants as const
+
+
+def main():
+    screen = pygame.display.set_mode(const.screen_size)
+    pygame.display.set_caption('BAO')
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        screen.fill(const.background_color)
+
+
+        pygame.display.update()
+
+main()
+
+
+
 
